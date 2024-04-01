af0e76c4e646c0c91bc5f627025c966faadb682f
Omkar Jadhav <162875896+Omkar-Pandurang-Jadhav@users.noreply.github.com>
2024-04-01 11:49:07 +0530

    Update AbstractionDemo.java

--- Abstraction/AbstractionDemo.java
+++ Abstraction/AbstractionDemo.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+abstract class Car{
+   public abstract void drive();
+   
+   public abstract void fly();
+   
+   public void playMusic(){
+     System.out.println("Play Music:");
+    }
+}
+
+abstract class WagonR extends Car{
+  public void drive(){
+   System.out.println("Driving:");
+  }
+  
+}
+class AeroHT extends WagonR{
+public void fly(){
+  System.out.println("Flying:");
+  }
+}
+
+public class AbstractionDemo{
+public static void main(String[] args){
+   Car obj=new AeroHT();
+   obj.drive();
+   obj.playMusic();
+   obj.fly();
+  }
+}
